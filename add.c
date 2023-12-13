abc386dadf667f88ce4e63730ac46e5c57604891
Tafara-N <c.tafaranyamhunga@gmail.com>
2023-12-13 14:20:07 +0200

    The opcode add adds the top two elements of the stack.

---
+++ add.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * adds - Function adds the top two elements of a stack
+ * @head: Head of the stack
+ * @cntr: Counts line number
+ *
+ * Return: Nothing
+ */
+
+void adds(stack_t **head, unsigned int cntr)
+{
+	stack_t *hd;
+	int leng = 0, aux;
+
+	hd = *head;
+
+	while (hd)
+	{
+		hd = hd->next;
+		leng++;
+	}
+
+	if (leng < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", cntr);
+		fclose(bus.fl);
+		free(bus.cnt);
+		freeStack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	hd = *head;
+	aux = hd->n + hd->next->n;
+	hd->next->n = aux;
+	*head = hd->next;
+	free(hd);
+}
