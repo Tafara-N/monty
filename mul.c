406f9186714d1d4e8b2598e7aa0bbabca39bd916
Tafara-N <c.tafaranyamhunga@gmail.com>
2023-12-13 14:22:47 +0200

    The opcode mul multiplies the second top element of the stack with the top element of the stack.

---
+++ mul.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * multi - Function multiplies the top two elements of a stack
+ * @head: Head of the stack
+ * @cntr: Counts line numbers
+ *
+ * Return: Nothing
+ */
+
+void multi(stack_t **head, unsigned int cntr)
+{
+	stack_t *hd;
+	int leng = 0, aux;
+
+	hd = *head;
+
+	while (hd)
+	{
+		hd = hd->next;
+		leng++;
+	}
+
+	if (leng < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", cntr);
+		fclose(bus.fl);
+		free(bus.cnt);
+		freeStack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	hd = *head;
+	aux = hd->next->n * hd->n;
+	hd->next->n = aux;
+	*head = hd->next;
+	free(hd);
+}
