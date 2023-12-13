2cf62d4b017fa1862b60f79b51467b250ac35580
Tafara-N <c.tafaranyamhunga@gmail.com>
2023-12-13 14:20:51 +0200

    The opcode nop doesn’t do anything.

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * none - Function returns nothing
+ * @head: Head of the stack
+ * @cntr: Counts line numbers
+ *
+ * Return: Nothing
+ */
+
+void none(stack_t **head, unsigned int cntr)
+{
+	(void) cntr;
+	(void) head;
+}
